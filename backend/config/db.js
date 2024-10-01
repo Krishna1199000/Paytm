c3f0af3026f1e8ed98b54aa11ec448cf043ceb07
krishna <gohilkrishna9004@gmail.com>
2024-10-01 21:46:49 +0530

    done

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbConnect = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB connected successfully"))
+    .catch(() => console.log("DB connection failed"));
+};
+
+module.exports = dbConnect;
+
