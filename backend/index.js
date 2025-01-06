26ae1126b395d4a4e426ae073da22576f1d6962a
krishna <gohilkrishna9004@gmail.com>
2025-01-06 18:45:38 +0530

    Done

---
+++ backend/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const path = require('path');
+
+const app = express();
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const accountRoutes = require("./routes/accountRoutes");
+
+const corsOptions = {
+  origin: 'http://localhost:5173', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  
+  credentials: true, 
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+require("dotenv").config();
+
+const dbConnect = require("./config/db");
+dbConnect();
+
+// Routes
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/account", accountRoutes);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// For Single Page Applications
+
+app.get("/", (req, res) => {
+  res.json({ message: "Your server is up and running..." });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong", error: err.message });
+});
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
